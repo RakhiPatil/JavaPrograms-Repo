77eea0f5e1e8cad93fb5ff64b2ff224dc0602564
RakhiPatil <rakhipatil2608@gmail.com>
2022-03-22 22:16:29 +0530

    ADDED: Printing Random Numbers Appearing on upperface of dice and print their sum

---
+++ src/com/RakhiPatil/Dices.java
@@ -0,0 +1,17 @@
+package com.RakhiPatil;
+
+import java.util.Random;
+
+public class Dices {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+
+		int p = (random.nextInt()%6 +1);
+		int q = (random.nextInt()%6 +1);
+		System.out.println("Values on Dice are : (" +p +" , "+q +")");
+		
+		int sum = p +q;
+		System.out.println("Sum :"+sum);
+	}
+}
